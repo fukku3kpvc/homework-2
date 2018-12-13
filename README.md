b241cf71c35eaa9d82c8136fb7203b0bdda70b89
fukk.u.3kpvc <ykwnd@yahoo.com>
2018-12-13 19:07:33 +0500

    Added second task

---
+++ README.md
@@ -0,0 +1,5 @@
+homework-2:
+    1. copy file from input dir to local dir
+    2. add to copied file comments on each line with number of chars
+    3. create dictionary from stations.txt
+    4. create ob-universe
---
+++ second.rb
@@ -0,0 +1,24 @@
+require 'fileutils'
+require 'date'
+
+print('Print file directory: ')
+dir = gets.chomp
+sum_len = 0
+if File.exist? File.expand_path(dir)
+  d = DateTime.now
+  format = d.year.to_s + d.month.to_s + d.day.to_s + d.hour.to_s + d.minute.to_s + d.second.to_s
+  file_out = File.new(Dir.pwd + '/' + format + File.basename(dir), 'w')
+  File.open(File.expand_path(dir)) do |f_in|
+    f_in.each do |line|
+      line.delete!("\n")
+      sum_len += line.length
+      file_out.puts('Длина строки ' + (line.length).to_s + ' символов')
+      file_out.puts(line)
+    end
+    file_out.puts('Общее количество символов - ' + sum_len.to_s)
+  end
+  file_out.close
+  puts 'Done!'
+else
+  puts 'No such file or dir'
+end
